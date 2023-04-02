602d4e67df1ea38c4ac56a71c2113d61e5af0117
Pep37917 <sarach37917@gmail.com>
2023-04-02 16:17:04 +0700

    Update Info.jsx

--- components/Info.jsx
+++ components/Info.jsx
@@ -5,7 +5,7 @@ export default function Info() {
                 <h2 className="name">John Mcsus</h2>
                 <h3 className="profession">Frontend Developer</h3>
             </div>
-            <img className="image" src="./img/prof-pic.png"></img>
+            <img className="image" src="/prof-pic.png"></img>
         </div>
     )
 }
